af0b02181fa7a3bd7dd0c6837050b89d2ae11dce
moskalevgeorge83-sys <moskalevgeorge83@gmail.com>
2025-12-02 13:20:43 +0500

    Add Recipe and RecipeDetail models

---
+++ models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Text
+from sqlalchemy.orm import relationship
+from database import Base
+
+
+class Recipe(Base):
+    __tablename__ = "recipes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True, nullable=False)
+    views = Column(Integer, default=0)
+    cook_time = Column(Integer, nullable=False)
+
+    details = relationship("RecipeDetail", back_populates="recipe",
+                           uselist=False, cascade="all, delete-orphan")
+
+
+class RecipeDetail(Base):
+    __tablename__ = "recipe_details"
+
+    id = Column(Integer, primary_key=True, index=True)
+    recipe_id = Column(Integer, ForeignKey("recipes.id"), nullable=False, unique=True)
+    name = Column(String, nullable=False)  # Дублируется для удобства
+    cook_time = Column(Integer, nullable=False)
+    ingredients = Column(Text, nullable=False)
+    description = Column(Text, nullable=True)
+
+    recipe = relationship("Recipe", back_populates="details")
